f00d00e125db9213c1e29b677f9eda3d9301aa00
leojiang <805329876@qq.com>
2024-10-14 21:18:47 +0800

    Update README.md

--- level1/p06_hanoi/main.cpp
+++ level1/p06_hanoi/main.cpp
@@ -1,6 +1,16 @@
-#include <printf.h>
-
+#include<iostream>
+using namespace std;
+int hanoi(int n, char a, char b, char c){
+  if(n==1) cout<<a<<"->"<<c<<endl;
+  else{
+      hanoi(n-1,a,c,b);
+      hanoi(1,a,b,c);
+      hanoi(n-1,b,a,c);
+  }
+}
 int main() {
-    printf("hello world!\n");
+    int n;
+    cin>>n;
+    hanoi(n, 'A', 'B', 'C');
     return 0;
 }
