489429e9f38fb647fc0d5a87d1a7b0fe5a82a704
leojiang00 <805329876@qq.com>
2024-12-29 17:26:33 +0800

    Add files via upload

--- level1/p06_hanoi/main.cpp
+++ level1/p06_hanoi/main.cpp
@@ -1,12 +1,16 @@
 #include<iostream>
 using namespace std;
 int hanoi(int n, char a, char b, char c){
-  if(n==1) cout<<a<<"->"<<c<<endl;
-  else{
-      hanoi(n-1,a,c,b);
-      hanoi(1,a,b,c);
-      hanoi(n-1,b,a,c);
-  }
+    if(n==1) {
+        cout<<a<<"->"<<c<<endl;
+        return 0;
+    }
+    else{
+        hanoi(n-1,a,c,b);
+        hanoi(1,a,b,c);
+        hanoi(n-1,b,a,c);
+        return 0;
+    }
 }
 int main() {
     int n;
